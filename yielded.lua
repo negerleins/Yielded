2d4c17bc6eef09f218bb7f7a94559d3dd1bbb37e
blacksmith <57269161+negerleins@users.noreply.github.com>
2024-11-08 20:28:28 -0800

    Update yielded.lua

--- yielded.lua
+++ yielded.lua
@@ -1,8 +1,7 @@
 -- // GUI TO LUA \\ --
 
--- // INSTANCES: 16 | SCRIPTS: 2 | MODULES: 0 \\ --
+-- // INSTANCES: 17 | SCRIPTS: 3 | MODULES: 0 \\ --
 
-local UI = {}
 local a = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
 if a:FindFirstChild("Yielded") then
 	a.Yielded:Destroy()
@@ -60,7 +59,7 @@ UI["7"]["FlexMode"] = Enum.UIFlexMode.Fill
 -- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UISizeConstraint \\ --
 UI["8"] = Instance.new("UISizeConstraint", UI["5"])
 UI["8"]["MinSize"] = Vector2.new(25, 25)
-UI["8"]["MaxSize"] = Vector2.new(728, 350.39999)
+UI["8"]["MaxSize"] = Vector2.new(391, 174)
 
 -- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent \\ --
 UI["9"] = Instance.new("Frame", UI["5"])
@@ -96,33 +95,39 @@ UI["c"]["Position"] = UDim2.new(0.5, 0, 1, -2)
 UI["c"]["BorderColor3"] = Color3.fromRGB(137, 137, 137)
 UI["c"]["Name"] = [[ResizeHandle_S]]
 
--- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_W \\ --
-UI["d"] = Instance.new("Frame", UI["9"])
-UI["d"]["ZIndex"] = 3
-UI["d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255)
-UI["d"]["AnchorPoint"] = Vector2.new(1, 0.5)
-UI["d"]["Size"] = UDim2.new(0, 4, 0, 4)
-UI["d"]["Position"] = UDim2.new(0, 2, 0.5, 0)
-UI["d"]["BorderColor3"] = Color3.fromRGB(137, 137, 137)
-UI["d"]["Name"] = [[ResizeHandle_W]]
+-- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_S.LocalScript \\ --
+UI["d"] = Instance.new("LocalScript", UI["c"])
 
--- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_N \\ --
+
+-- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_W \\ --
 UI["e"] = Instance.new("Frame", UI["9"])
+UI["e"]["Visible"] = false
 UI["e"]["ZIndex"] = 3
 UI["e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255)
-UI["e"]["AnchorPoint"] = Vector2.new(0.5, 1)
+UI["e"]["AnchorPoint"] = Vector2.new(1, 0.5)
 UI["e"]["Size"] = UDim2.new(0, 4, 0, 4)
-UI["e"]["Position"] = UDim2.new(0.5, 0, 0, 2)
+UI["e"]["Position"] = UDim2.new(0, 2, 0.5, 0)
 UI["e"]["BorderColor3"] = Color3.fromRGB(137, 137, 137)
-UI["e"]["Name"] = [[ResizeHandle_N]]
+UI["e"]["Name"] = [[ResizeHandle_W]]
+
+-- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_N \\ --
+UI["f"] = Instance.new("Frame", UI["9"])
+UI["f"]["Visible"] = false
+UI["f"]["ZIndex"] = 3
+UI["f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255)
+UI["f"]["AnchorPoint"] = Vector2.new(0.5, 1)
+UI["f"]["Size"] = UDim2.new(0, 4, 0, 4)
+UI["f"]["Position"] = UDim2.new(0.5, 0, 0, 2)
+UI["f"]["BorderColor3"] = Color3.fromRGB(137, 137, 137)
+UI["f"]["Name"] = [[ResizeHandle_N]]
 
 -- // Players.CheckCashedV8.PlayerGui.Yielded.UIScale \\ --
-UI["f"] = Instance.new("UIScale", UI["1"])
+UI["10"] = Instance.new("UIScale", UI["1"])
 
 
 -- // Players.CheckCashedV8.PlayerGui.Yielded.UIScale.AutoScale \\ --
-UI["10"] = Instance.new("LocalScript", UI["f"])
-UI["10"]["Name"] = [[AutoScale]]
+UI["11"] = Instance.new("LocalScript", UI["10"])
+UI["11"]["Name"] = [[AutoScale]]
 
 -- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_E.LocalScript \\ --
 local function SCRIPT_b()
@@ -267,13 +272,14 @@ local script = UI["b"]
 	
 	Input.InputChanged:Connect(function(input: InputObject, gameProcessedEvent: boolean)
 		if Pressed and SavedPosition then
+			local ScreenGui = script:FindFirstAncestorWhichIsA("ScreenGui")
 			local Frame = script.Parent.Parent.Parent;
-			local UIScale = script.Parent.Parent.Parent.Parent.Parent.UIScale
+			local UIScale = ScreenGui.UIScale
 	
 			local screenWidth = workspace.CurrentCamera.ViewportSize.X
 			local Calc = math.clamp(Mouse.X / UIScale.Scale, 0, screenWidth)
-	
-			local SavedPosition = Vector2.new(Calc, Frame.AbsoluteSize.Y)
+			
+			local SavedPosition = Vector2.new(Calc, Frame.UISizeConstraint.MaxSize.Y)
 	
 			game.TweenService:Create(
 				Frame.UISizeConstraint,
@@ -290,9 +296,176 @@ local script = UI["b"]
 	end)
 end
 task.spawn(SCRIPT_b)
+-- // Players.CheckCashedV8.PlayerGui.Yielded.Screen.Frame.UIEditorResizeHandleParent.ResizeHandle_S.LocalScript \\ --
+local function SCRIPT_d()
+local script = UI["d"]
+	local Input = game:GetService("UserInputService")
+	local Content = game:GetService("ContentProvider");
+	local Insert = game:GetService("InsertService");
+	local Players = game:GetService("Players")
+	local Mouse = Players.LocalPlayer:GetMouse()
+	
+	local SavedPosition = Vector2.new();
+	local MouseEntered, Pressed = false, false;
+	local DefaultMouseIcon = Input.MouseIcon
+	
+	local function AutoScaleGui(
+		uiScale: UIScale, 
+		screenGui: ScreenGui, 
+		baseScale: number,
+		sizeFromCurrent: boolean
+	)
+		local currentResolutionX = screenGui.AbsoluteSize.X;
+		local currentResolutionY = screenGui.AbsoluteSize.Y;
+		
+		local baseResolutionX = sizeFromCurrent and currentResolutionX or 1920;
+		local baseResolutionY = sizeFromCurrent and currentResolutionY or 1080;
+		local baseScale = 1;
+		
+		local scaleFactorX = currentResolutionX / baseResolutionX;
+		local scaleFactorY = currentResolutionY / baseResolutionY;
+		local averageScaleFactor = (scaleFactorX + scaleFactorY) / 2;
+	
+		uiScale.Scale = math.clamp(baseScale * averageScaleFactor, 1, math.huge);
+	end
+	
+	local function CustomCursor(SizeInOffset: number)
+		type Class = {
+			ScreenGui: ScreenGui,
+			ImageLabel: ImageLabel,
+			UIScale: UIScale,
+			Connection: RBXScriptConnection,
+			DefaultCursor: string
+		};
+		
+		type Meta = {
+			set: (self: Class, Image: string) -> nil,
+			default: (self: Class) -> nil,
+			toggle: (self: Class) -> nil,
+			disable: (self: Class) -> nil
+		} & Class;
+		
+		local data = {
+			set = function(self: Meta, Image: string)
+				Content:PreloadAsync({ Image });
+				self.ImageLabel.Image = Image;
+			end,
+			default = function(self: Meta)
+				self.ImageLabel.Image = self.DefaultCursor;
+			end,
+			toggle = function(self: Meta)
+				self.ImageLabel.Visible = not self.ImageLabel.Visible
+			end,
+			disable = function(self: Meta)
+				self.Connection:Disconnect();
+				self.ScreenGui:Destroy();
+				
+				Input.MouseIconEnabled = true;
+			end
+		};
+		
+		data.__index = data;
+		data.__metatable = "getmetatable() -> Is not allowed.";
+		
+		return setmetatable((function(this: Meta)
+			Input.MouseIconEnabled = false;
+			
+			local Parent: Instance = game:FindFirstChild("CoreGui") 
+				or game.Players.LocalPlayer.PlayerGui;
+			
+			if Parent:FindFirstChild("CustomCursor") then
+				Parent.CustomCursor:Destroy();
+			end;
+			
+			this.ScreenGui = Instance.new("ScreenGui", Parent);
+			this.ScreenGui.Name = "CustomCursor";
+			this.ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
+			this.ScreenGui.DisplayOrder = 99999999;
+			
+			this.UIScale = Instance.new("UIScale", this.ScreenGui)
+			AutoScaleGui(this.UIScale, this.ScreenGui, 1, false)
+			
+			this.ImageLabel = Instance.new("ImageLabel", this.ScreenGui);
+			this.ImageLabel.BackgroundTransparency = 1;
+			this.ImageLabel.Interactable = false;
+			this.ImageLabel.Image = this.DefaultCursor;
+			this.ImageLabel.Size = UDim2.fromOffset(SizeInOffset, SizeInOffset);
+			
+			this.Connection = Input.InputChanged:Connect(function(input: InputObject)
+				if input.UserInputType == Enum.UserInputType.MouseMovement then
+					this.ImageLabel.Position = UDim2.fromOffset(
+						Mouse.X - SizeInOffset / 2,
+						Mouse.Y - SizeInOffset / 2
+					);
+				end;
+			end);
+			
+			return this;
+		end)({ 
+			ScreenGui = nil, 
+			ImageLabel = nil, 
+			UIScale = nil,
+			Connection = nil,
+			DefaultCursor = "rbxasset://textures/ArrowFarCursor.png"
+		}), data) :: Class;
+	end;
+	
+	local newCursor = CustomCursor(50);
+	
+	script.Parent.MouseEnter:Connect(function()
+		MouseEntered = true
+		newCursor:set("rbxasset://textures/StudioUIEditor/icon_resize4.png")
+	end)
+	
+	script.Parent.MouseLeave:Connect(function()
+		MouseEntered = false
+	end)
+	
+	
+	Input.InputBegan:Connect(function(input: InputObject, gameProcessedEvent: boolean)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			if MouseEntered and not Pressed then
+				Pressed = true
+				SavedPosition = Vector2.new(Mouse.X, 0)
+			end
+		end
+	end)
+	
+	Input.InputEnded:Connect(function(input: InputObject, gameProcessedEvent: boolean)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			Pressed = false
+		end
+	end)
+	
+	Input.InputChanged:Connect(function(input: InputObject, gameProcessedEvent: boolean)
+		if Pressed and SavedPosition then
+			local ScreenGui = script:FindFirstAncestorWhichIsA("ScreenGui")
+			local Frame = script.Parent.Parent.Parent;
+			local UIScale = ScreenGui.UIScale
+	
+			local screenWidth = workspace.CurrentCamera.ViewportSize.Y
+			local Calc = math.clamp(Mouse.Y / UIScale.Scale, 0, screenWidth)
+			
+			local SavedPosition = Vector2.new(Frame.UISizeConstraint.MaxSize.X, Calc)
+	
+			game.TweenService:Create(
+				Frame.UISizeConstraint,
+				TweenInfo.new(0.15),
+				{
+					MaxSize = SavedPosition
+				}
+			):Play()
+		end;
+		
+		if not Pressed and not MouseEntered then
+			newCursor:default()
+		end;
+	end)
+end
+task.spawn(SCRIPT_d)
 -- // Players.CheckCashedV8.PlayerGui.Yielded.UIScale.AutoScale \\ --
-local function SCRIPT_10()
-local script = UI["10"]
+local function SCRIPT_11()
+local script = UI["11"]
 	local ScreenGui = script:FindFirstAncestorWhichIsA('ScreenGui');
 	
 	ScreenGui:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
@@ -319,6 +492,6 @@ local script = UI["10"]
 	
 	script.Parent.Scale = math.clamp(baseScale * averageScaleFactor, 1, math.huge)
 end
-task.spawn(SCRIPT_10)
+task.spawn(SCRIPT_11)
 
 return UI["1"], require;
